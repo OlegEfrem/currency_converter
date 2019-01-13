216ac28987c07002d30c234c6a73ca37be98a36f
Oleg Efrem <oleg.efrem@yahoo.com>
2019-01-13 19:44:13 +0000

    Add jsonToMap failing test for empty input

--- src/test/scala/com/oef/converter/json/jackson/JsonConverterJacksonTest.scala
+++ src/test/scala/com/oef/converter/json/jackson/JsonConverterJacksonTest.scala
@@ -3,14 +3,28 @@ import java.io.InputStream
 
 import com.oef.converter.UnitSpec
 
-
 class JsonConverterJacksonTest extends UnitSpec {
   "jsonToMap should" - {
-    "return empty for empty input" in {
-      val jsonConverter = new JsonConverterJackson
-      jsonConverter.jsonToMap(readFile("JsonSample.json")) shouldBe Map()
+    val jsonConverter = new JsonConverterJackson
+    import JsonConverterJacksonTest._
+
+    "return empty for an empty json" in {
+      jsonConverter.jsonToMap(emptyJson) shouldBe Map()
     }
+
+    "return an IllegalArgumentException for an empty input" in {
+      a[IllegalArgumentException] should be thrownBy jsonConverter.jsonToMap(emptyFile)
+    }
+
   }
+
+}
+
+object JsonConverterJacksonTest {
+  val emptyJson: InputStream   = readFile("Empty.json")
+  val emptyFile: InputStream   = readFile("EmptyFile")
+  val invalidJson: InputStream = readFile("InvalidJson")
+  val validJson: InputStream   = readFile("3Level.json")
 
   private def readFile(fileName: String): InputStream = getClass.getResourceAsStream(fileName)
 }
