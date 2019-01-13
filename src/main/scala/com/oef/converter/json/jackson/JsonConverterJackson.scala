9bf92d0ed906afd58ed86efa5032baaa8d1933a2
Oleg Efrem <oleg.efrem@yahoo.com>
2019-01-13 19:28:30 +0000

    Pass jsonToMap first simplest failing test for empty case (TDD style)

--- src/main/scala/com/oef/converter/json/jackson/JsonConverterJackson.scala
+++ src/main/scala/com/oef/converter/json/jackson/JsonConverterJackson.scala
@@ -4,5 +4,5 @@ import java.io.InputStream
 import com.oef.converter.json.JsonConverter
 
 class JsonConverterJackson extends JsonConverter {
-  def jsonToMap(stream: InputStream): Map[String,Any] = ???
+  def jsonToMap(stream: InputStream): Map[String,Any] = Map()
 }
