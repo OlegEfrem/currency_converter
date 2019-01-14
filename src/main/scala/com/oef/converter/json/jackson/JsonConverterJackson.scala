6d20c97a56a16df2da34106138585a0015d084df
Oleg Efrem <oleg.efrem@yahoo.com>
2019-01-14 11:44:19 +0000

    Pass jsonToMap failing test for invalid json

--- src/main/scala/com/oef/converter/json/jackson/JsonConverterJackson.scala
+++ src/main/scala/com/oef/converter/json/jackson/JsonConverterJackson.scala
@@ -1,11 +1,22 @@
 package com.oef.converter.json.jackson
 import java.io.InputStream
 
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.oef.converter.json.JsonConverter
 
 class JsonConverterJackson extends JsonConverter {
-  def jsonToMap(stream: InputStream): Map[String, Any] = {
-    if (stream.available() == 0) throw new IllegalArgumentException("Empty data is not a valid json.")
-    Map()
+  private val mapper = new ObjectMapper() with ScalaObjectMapper
+  mapper.registerModule(DefaultScalaModule)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  def jsonToMap(json: InputStream): Map[String, Any] = {
+    if (json.available() == 0) throw new IllegalArgumentException("Empty data is not a valid json.")
+    else {
+      mapper.readTree(json)
+      Map()
+    }
+
   }
 }
